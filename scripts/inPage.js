937634d24a8f739b4d60f8a018ae20544e89b2ec
Deveshb15 <deveshb583@gmail.com>
2024-05-02 00:26:25 +0530

    display the tokens on the warpcast

--- scripts/inPage.js
+++ scripts/inPage.js
@@ -4,35 +4,59 @@ let observer = new MutationObserver(() => {
 
 observer.observe(document, { subtree: true, childList: true });
 
-function onChange() {
+const getNumberInKMFormat = (num) => {
+    if (num > 999 && num < 1000000) {
+        return (num / 1000).toFixed(1) + 'K'; // convert to K for number from > 1000 < 1 million
+    } else if (num > 1000000) {
+        return (num / 1000000).toFixed(1) + 'M'; // convert to M for number from > 1 million
+    } else if (num < 900) {
+        return num?.toFixed(1); // if value < 1000, nothing to do
+    }
+}
+
+async function onChange() {
     console.log("URL CHANGED");
     let parentHoverDiv = document.querySelector('[data-radix-popper-content-wrapper]');
 
     if (parentHoverDiv) {
         let hoverDiv = parentHoverDiv?.firstChild?.firstChild?.firstChild?.firstChild
         if (hoverDiv) {
-            // let parentAppendDiv = hoverDiv?.lastChild?.lastChild?.previousElementSibling?.previousElementSibling
-            let parentAppendDiv = hoverDiv?.lastChild?.firstChild?.nextSibling
-            // Check if our custom header is already added
-            if (!parentAppendDiv.querySelector('.custom-header')) {
+            let appendParentDiv = hoverDiv?.lastChild
 
-                let name = hoverDiv?.lastChild?.firstChild?.firstChild?.innerText
+            // Check if our custom header is already added
+            if (!appendParentDiv.querySelector('.custom-header')) {
+
                 let username = hoverDiv?.lastChild?.firstChild?.lastChild?.innerText
                 console.log("Username: ", username);
+
                 observer.disconnect(); // Stop observing
-                
-                let test = document.createElement("div");
-                test.innerHTML = `<h1 class="custom-header text-extrabold text-lg">Test ext - ${username}</h1>`; // give it a class so we can identify it later
-                let testDiv = document.createElement("div");
-                testDiv.appendChild(test);
 
-                // <div><a class="relative truncate text-base font-semibold text-default hover:underline" title="Joe Blau 🎩" href="/joeblau" tabindex="-1">Joe Blau 🎩</a><div class="flex flex-row items-baseline space-x-2"><a class="relative text-muted hover:underline" title="Joe Blau 🎩" href="/joeblau" tabindex="-1">@joeblau</a></div></div>
+                let appendDiv = document.createElement("div");
+                appendDiv.classList.add('mt-2', 'flex', 'flex-row', 'gap-2', 'custom-header');
+                appendDiv.innerHTML = `<a tabindex="-1"><span class="mr-1 font-semibold text-default">Loading...</span></a>`
+                // add after appendParentDiv
+                // parentAppendDiv.insertBefore(appendDiv, );
+                appendParentDiv.insertBefore(appendDiv, appendParentDiv?.lastChild);
 
-                
-                parentAppendDiv.appendChild(testDiv);
-                
+                // https://extension-backend-steel.vercel.app/user-data/prberg
+                if (username?.length > 0) {
+                    try {
+                        console.log("Fetching data for: ", username);
+                        let response = await fetch(`https://extension-backend-steel.vercel.app/user-data/${username?.slice(1)}`);
+                        console.log("Response: ", response);
+                        let data = await response.json();
+                        console.log("Data: ", data);
+                        appendDiv.innerHTML = `<a tabindex="-1"><span class="mr-1 font-semibold text-default">$${getNumberInKMFormat(data?.total_value)}</span><span style="color: ${data?.pnl > 0 ? 'green' : 'red'} " class="mr-1 text-sm ">${data?.pnl?.toFixed(2)}</span> <span class="text-muted text-semibold">Net Worth</span></a><a tabindex="-1"><span class="mr-1 font-semibold text-default">$${getNumberInKMFormat(data?.total_nft_portfolio)}</span><span class="text-muted text-semibold">NFTs</span></a>`
+
+                    } catch (error) {
+                        console.error("Error: ", error);
+                    }
+                }
+                // setTimeout(() => {
+                // }, 2000)
+
                 observer.observe(document, { subtree: true, childList: true }); // Start observing again
-            }   
+            }
         }
     }
 }
